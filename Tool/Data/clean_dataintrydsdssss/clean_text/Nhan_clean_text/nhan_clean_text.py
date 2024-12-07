1caa9606cd67a127699679d80ad11f3274cd1fbc
dangnhan2309 <dangnhan2309@gmail.com>
2024-12-07 11:27:04 +0700

    nashdasbdasjds

--- Tool/Data/clean_dataintrydsdssss/clean_text/Nhan_clean_text/nhan_clean_text.py
+++ Tool/Data/clean_dataintrydsdssss/clean_text/Nhan_clean_text/nhan_clean_text.py
@@ -1,9 +1,38 @@
 import re
+from nltk.corpus import stopwords
+import string
+
+
 
 
 class clean_text: 
     def __init__(self,text):
         self.text =text 
     def clean_extra(self):
+        self.text = re.sub(r'[^A-Za-z0-9\s]', '', self.text)
+
+
+        nltk.download('stopwords')
+        stop_words = set(stopwords.words('english'))
+        filtered_text = ' '.join([word for word in self.text.split() if word.lower() not in stop_wordsx])
+        
+        self.text = self.text.translate(str.maketrans('', '', string.punctuation))
+
+
+        return self.text 
+
+def main(): 
+
+
+
+if __name__ == "__main__":
+    main()
+        
+
+
+
+
+
+
 
         
